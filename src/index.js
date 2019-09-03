1de4485392a1d018ea407eb945d500e431adebe3
Aleksandr V. Alekseev <ci7alex1@gmail.com>
2019-09-03 21:58:31 +0300

    Almost done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+document.title = "TAIS REP for IFLY";
 document.getElementById("app").innerHTML = "";
+//document.getElementById("frmAirlineCode").style.width = "2rem";
+//document.getElementById("frmAirportFrom").style.width = "5rem";
+//document.getElementById("frmAirportTo").style.width = "5rem";
