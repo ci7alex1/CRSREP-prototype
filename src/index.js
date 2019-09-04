6e57804e57d63bc79f8c690f74c1e40a69885a30
Aleksandr V. Alekseev <ci7alex1@gmail.com>
2019-09-04 13:21:08 +0300

    Added toggle to fields

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ document.title = "TAIS REP for IFLY";
 document.getElementById("app").innerHTML = "";
 //document.getElementById("frmAirlineCode").style.width = "2rem";
 //document.getElementById("frmAirportFrom").style.width = "5rem";
-//document.getElementById("frmAirportTo").style.width = "5rem";
+//document.getElementById("frmDocumentNo").onmouseover = ShowLabel();
+//document.getElementById("frmDocumentNo").onblur = HideLabel();
